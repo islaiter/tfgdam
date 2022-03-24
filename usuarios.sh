291c932ea0c703801c46c0211d1036e5afad14fc
islaiter <45145344+islaiter@users.noreply.github.com>
2022-03-24 15:46:01 +0100

    Update usuarios.sh

--- usuarios.sh
+++ usuarios.sh
@@ -2,3 +2,13 @@
 
 sudo useradd -m -p maniana1234 maniana
 sudo useradd -m -p tarde1234 tarde
+
+# Como los creamos para samba, le asignamos una contraseña en linux
+
+sudo echo "maniana:maniana1234" | sudo chpasswd
+sudo echo "tarde:tarde1234" | sudo chpasswd
+
+# Ahora les tenemos que asignar una shell en linux
+
+sudo chsh -s /bin/bash maniana
+sudo chsh -s /bin/bash tarde
