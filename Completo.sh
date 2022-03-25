6f8546f1d9bc6067b6a94adcca45dcb429285854
islaiter <45145344+islaiter@users.noreply.github.com>
2022-03-25 14:56:07 +0100

    Update Completo.sh
    Modificando los usuarios y añadimos al grupo docker

--- Completo.sh
+++ Completo.sh
@@ -52,6 +52,19 @@ echo “Creando usuarios…”
 sudo useradd -m -p maniana1234 maniana
 sudo useradd -m -p tarde1234 tarde
 
+# Le asignamos contraseña en linux, ya que los creamos compatibles con samba
+
+sudo echo "maniana:maniana1234" | sudo chpasswd
+sudo echo "tarde:tarde1234" | sudo chpasswd
+
+sudo chsh -s /bin/bash maniana
+sudo chsh -s /bin/bash tarde
+
+# Añadimos los usuarios al grupo docker para que ejecuten comandos
+
+sudo gpasswd -a maniana docker
+sudo gpasswd -a tarde docker
+
 echo “Usuarios creados correctamente”
 
 # Ahora queremos instalar docker
