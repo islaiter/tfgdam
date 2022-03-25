f761d45a1edfab997416e457e46a18c8a3a28cf6
islaiter <45145344+islaiter@users.noreply.github.com>
2022-03-25 14:56:23 +0100

    Update usuarios.sh
    Los añadimos al grupo docker

--- usuarios.sh
+++ usuarios.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-
 sudo useradd -m -p maniana1234 maniana
 sudo useradd -m -p tarde1234 tarde
 
-# Como los creamos para samba, le asignamos una contraseña en linux
+# Le asignamos contraseña en linux, ya que los creamos compatibles con samba
 
 sudo echo "maniana:maniana1234" | sudo chpasswd
 sudo echo "tarde:tarde1234" | sudo chpasswd
 
-# Ahora les tenemos que asignar una shell en linux
-
 sudo chsh -s /bin/bash maniana
 sudo chsh -s /bin/bash tarde
+
+# Añadimos los usuarios al grupo docker para que ejecuten comandos
+
+sudo gpasswd -a maniana docker
+sudo gpasswd -a tarde docker
