12d0e414d294e2e0ca5ea408aa8e77afe9fcd1e4
islaiter <45145344+islaiter@users.noreply.github.com>
2022-03-30 15:06:32 +0200

    Update servicioDocker.sh

--- servicioDocker.sh
+++ servicioDocker.sh
@@ -8,8 +8,17 @@ sudo cp /lib/systemd/system/docker.service /lib/systemd/system/docker-maniana.se
 
 sudo cp /lib/systemd/system/docker.service /lib/systemd/system/docker-tarde.service
 
-sed -i "s/ExecStart=/usr/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock/ExecStart=/usr/bin/dockerd --host unix:///var/run/docker.sock --pidfile /var/run/docker.pid" /lib/systemd/system/docker.service
+ExecStart=/usr/bin/dockerd \
+          --host unix:///var/run/docker.sock \
+          --pidfile /var/run/docker.pid \
+          
 
-sed -i "s/ExecStart=/usr/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock/ExecStart=/usr/bin/dockerd --userns-remap maniana --host unix:///var/run/docker-maniana.sock --pidfile /var/run/docker-maniana.pid" /lib/systemd/system/docker-maniana.service
-
-sed -i "s/ExecStart=/usr/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock/ExecStart=/usr/bin/dockerd --userns-remap tarde --host unix:///var/run/docker-tarde.sock --pidfile /var/run/docker-tarde.pid " /lib/systemd/system/docker-tarde.service
+ExecStart=/usr/bin/dockerd \
+          --userns-remap maniana \
+          --host unix:///var/run/docker-maniana.sock \
+          --pidfile /var/run/docker-maniana.pid \
+        
+ExecStart=/usr/bin/dockerd \
+          --userns-remap tarde \
+          --host unix:///var/run/docker-tarde.sock \
+          --pidfile /var/run/docker-tarde.pid \
