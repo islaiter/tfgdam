9c6f6d086885882114b563fc7d01090868eff380
islaiter <45145344+islaiter@users.noreply.github.com>
2022-03-31 09:40:04 +0200

    Update servicioDocker.sh
    Añadiendo los alias para finalizar

--- servicioDocker.sh
+++ servicioDocker.sh
@@ -8,7 +8,6 @@ Documentation=https://docs.docker.com
 After=network-online.target docker.socket firewalld.service containerd.service
 Wants=network-online.target
 Requires=docker.socket containerd.service
-
 [Service]
 Type=notify
 # the default is not to use systemd for cgroups because the delegate issues still
@@ -17,43 +16,34 @@ Type=notify
 # ExecStart=/usr/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock
 #ExecStart=
 #ExecStart=/usr/bin/dockerd
-
 ExecStart=/usr/bin/dockerd \
           --host unix:///var/run/docker.sock \
-          --pidfile /var/run/docker.pid \
-
+          --pidfile /var/run/docker.pid
 ExecReload=/bin/kill -s HUP $MAINPID
 TimeoutSec=0
 RestartSec=2
 Restart=always
-
 # Note that StartLimit* options were moved from "Service" to "Unit" in systemd 229.
 # Both the old, and new location are accepted by systemd 229 and up, so using the old location
 # to make them work for either version of systemd.
 StartLimitBurst=3
-
 # Note that StartLimitInterval was renamed to StartLimitIntervalSec in systemd 230.
 # Both the old, and new name are accepted by systemd 230 and up, so using the old name to make
 # this option work for either version of systemd.
 StartLimitInterval=60s
-
 # Having non-zero Limit*s causes performance problems due to accounting overhead
 # in the kernel. We recommend using cgroups to do container-local accounting.
 LimitNOFILE=infinity
 LimitNPROC=infinity
 LimitCORE=infinity
-
 # Comment TasksMax if your systemd version does not support it.
 # Only systemd 226 and above support this option.
 TasksMax=infinity
-
 # set delegate yes so that systemd does not reset the cgroups of docker containers
 Delegate=yes
-
 # kill only the docker process, not all processes in the cgroup
 KillMode=process
 OOMScoreAdjust=-500
-
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -67,7 +57,6 @@ Documentation=https://docs.docker.com
 After=network-online.target docker.socket firewalld.service containerd.service
 Wants=network-online.target
 Requires=docker.socket containerd.service
-
 [Service]
 Type=notify
 # the default is not to use systemd for cgroups because the delegate issues still
@@ -76,44 +65,35 @@ Type=notify
 # ExecStart=/usr/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock
 #ExecStart=
 #ExecStart=/usr/bin/dockerd
-
 ExecStart=/usr/bin/dockerd \
           --userns-remap maniana \
           --host unix:///var/run/docker-maniana.sock \
-          --pidfile /var/run/docker-maniana.pid \
-
+          --pidfile /var/run/docker-maniana.pid
 ExecReload=/bin/kill -s HUP $MAINPID
 TimeoutSec=0
 RestartSec=2
 Restart=always
-
 # Note that StartLimit* options were moved from "Service" to "Unit" in systemd 229.
 # Both the old, and new location are accepted by systemd 229 and up, so using the old location
 # to make them work for either version of systemd.
 StartLimitBurst=3
-
 # Note that StartLimitInterval was renamed to StartLimitIntervalSec in systemd 230.
 # Both the old, and new name are accepted by systemd 230 and up, so using the old name to make
 # this option work for either version of systemd.
 StartLimitInterval=60s
-
 # Having non-zero Limit*s causes performance problems due to accounting overhead
 # in the kernel. We recommend using cgroups to do container-local accounting.
 LimitNOFILE=infinity
 LimitNPROC=infinity
 LimitCORE=infinity
-
 # Comment TasksMax if your systemd version does not support it.
 # Only systemd 226 and above support this option.
 TasksMax=infinity
-
 # set delegate yes so that systemd does not reset the cgroups of docker containers
 Delegate=yes
-
 # kill only the docker process, not all processes in the cgroup
 KillMode=process
 OOMScoreAdjust=-500
-
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -127,7 +107,6 @@ Documentation=https://docs.docker.com
 After=network-online.target docker.socket firewalld.service containerd.service
 Wants=network-online.target
 Requires=docker.socket containerd.service
-
 [Service]
 Type=notify
 # the default is not to use systemd for cgroups because the delegate issues still
@@ -136,45 +115,60 @@ Type=notify
 # ExecStart=/usr/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock
 #ExecStart=
 #ExecStart=/usr/bin/dockerd
-
 ExecStart=/usr/bin/dockerd \
           --userns-remap tarde \
           --host unix:///var/run/docker-tarde.sock \
-          --pidfile /var/run/docker-tarde.pid \
-
+          --pidfile /var/run/docker-tarde.pid
 ExecReload=/bin/kill -s HUP $MAINPID
 TimeoutSec=0
 RestartSec=2
 Restart=always
-
 # Note that StartLimit* options were moved from "Service" to "Unit" in systemd 229.
 # Both the old, and new location are accepted by systemd 229 and up, so using the old location
 # to make them work for either version of systemd.
 StartLimitBurst=3
-
 # Note that StartLimitInterval was renamed to StartLimitIntervalSec in systemd 230.
 # Both the old, and new name are accepted by systemd 230 and up, so using the old name to make
 # this option work for either version of systemd.
 StartLimitInterval=60s
-
 # Having non-zero Limit*s causes performance problems due to accounting overhead
 # in the kernel. We recommend using cgroups to do container-local accounting.
 LimitNOFILE=infinity
 LimitNPROC=infinity
 LimitCORE=infinity
-
 # Comment TasksMax if your systemd version does not support it.
 # Only systemd 226 and above support this option.
 TasksMax=infinity
-
 # set delegate yes so that systemd does not reset the cgroups of docker containers
 Delegate=yes
-
 # kill only the docker process, not all processes in the cgroup
 KillMode=process
 OOMScoreAdjust=-500
-
 [Install]
 WantedBy=multi-user.target
 EOF
+echo "Archivos configurados"
 
+echo “Aplicando cambio a dockerd”
+
+systemctl daemon-reload
+
+echo "reload relizado"
+systemctl restart docker
+echo "restart realizado"
+
+echo “Cambios aplicados con exito”
+
+echo “Configurando el archivo bashrc…”
+
+echo "alias docker="sudo docker -H unix:///var/run/docker-maniana.sock"" | sudo tee -a /home/maniana/.bashrc
+
+
+echo "alias docker="sudo docker -H unix:///var/run/docker-tarde.sock"" | sudo tee -a /home/tarde/.bashrc
+
+
+echo “Archivo bashrc configurado correctamente”
+
+echo “Ya hemos terminado de configurar todo”
+
+echo "Configurado todo correctamente"
