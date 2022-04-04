fc3196d2121797f94aaf33ea4db57b6ca27ba8c4
islaiter <45145344+islaiter@users.noreply.github.com>
2022-04-04 12:14:57 +0200

    Create CompletoWindows.bat
    Inicio del script, podemos crear usuarios

---
+++ CompletoWindows.bat
@@ -0,0 +1,8 @@
+:: Script que automatizara toda la infrasestructura del proyecto para Windows
+
+:: Creamos los usuarios maniana y tarde con las contraseñas correspondientes
+
+REM Creando los usuarios
+
+net user maniana maniana1234 /ADD
+net user maniana tarde1234 /ADD
