ce93b4cd15aee46e694ea53b1059f040930b1dd9
islaiter <45145344+islaiter@users.noreply.github.com>
2022-04-05 08:24:16 +0200

    Update CompletoWindows.bat
    Añadiendo la descarga de winget para que podamos descargarnos las aplicaciones de la windows store

--- CompletoWindows.bat
+++ CompletoWindows.bat
@@ -23,3 +23,6 @@ powershell -Command "& {Install-Module PSWindowsUpdate}"
 
 powershell -Command "& {Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main' -Name "DisableFirstRunCustomize" -Value 2}"
 
+:: Añadimos la descarga de winget para obtener aplicaciones de windows store
+
+Start-BitsTransfer -Source 'https://github.com/microsoft/winget-cli/releases/download/v1.2.10271/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'  -Destination "C:\Users\admin\Desktop\winget.msixbundle";
